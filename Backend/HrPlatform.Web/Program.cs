800172d9ed1278af2ef25d6dc7dc77a191b6cf96
HILTON <artyomhilton@yahoo.com>
2025-11-23 00:54:45 +0300

    Initial project

---
+++ Backend/HrPlatform.Web/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("swagger/v1/index.html", "V1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseHttpsRedirection();
+
+await app.RunAsync();
