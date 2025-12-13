bf23fc9b82ab6d4df3f4e58770355b7741d998ef
artyomhilton <artyomhilton@gmail.com>
2025-12-14 00:51:15 +0300

    Добавлен глобальный оработчик исключенийэ

---
+++ Backend/HrPlatform.Web/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace HrPlatform.Web.Middleware;
+
+public class GlobalExceptionHandlerMiddleware : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+    public GlobalExceptionHandlerMiddleware(ILogger<GlobalExceptionHandlerMiddleware> logger)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var problemDetails = exception switch
+        {
+            NpgsqlException => CreateProblemDetails(exception.GetType().Name, "NotDbConnection", StatusCodes.Status500InternalServerError, exception.HelpLink),
+            Exception => CreateProblemDetails(exception.GetType().Name, exception.Message, StatusCodes.Status500InternalServerError, exception.HelpLink)
+        };
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+
+    private ProblemDetails CreateProblemDetails(string title, string detail, int statusCode, string? type) =>
+        new ProblemDetails()
+        {
+            Title = title,
+            Detail = detail,
+            Status = statusCode,
+            Type = type,
+        };
+}
